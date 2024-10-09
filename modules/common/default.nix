5174ed3c496cc37f85d285a9874756992e43f91d
a-clean-kitchen <Mashaw.Quade@gmail.com>
2024-10-09 16:48:57 -0400

    small tweaks

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -65,29 +65,41 @@ in {
     stateVersion = "23.05";
   in {
     
-    # Basic common system packages for all devices
-    environment.systemPackages = with pkgs; [ git tldr wget curl ];
+    environment = {
+      sessionVariables = {
+        # These are the defaults, and xdg.enable does set them, but due to load
+        # order, they're not set before environment.variables are set, which could
+        # cause race conditions.
+        XDG_CACHE_HOME  = "$HOME/.cache";
+        XDG_CONFIG_HOME = "$HOME/.config";
+        XDG_DATA_HOME   = "$HOME/.local/share";
+        XDG_BIN_HOME    = "$HOME/.local/bin";
+      };
+      systemPackages = with pkgs; [ git tldr wget curl ];
+    };
 
-    # Use the system-level nixpkgs instead of Home Manager's
-    home-manager.useGlobalPkgs = true;
+    home-manager = {
+      # Use the system-level nixpkgs instead of Home Manager's
+      useGlobalPkgs = true;
+      
+      # Install packages to /etc/profiles instead of ~/.nix-profile, useful when
+      # using multiple profiles for one user
+      useUserPackages = true;
+      users = {
+        ${config.user} = {
+          # Pin a state version to prevent warnings
+          home.stateVersion = stateVersion;
+          programs.home-manager.enable = true;
+        };
+        root.home.stateVersion = stateVersion;
+      };
 
-    # Install packages to /etc/profiles instead of ~/.nix-profile, useful when
-    # using multiple profiles for one user
-    home-manager.useUserPackages = true;
+    };
 
     # Allow specified unfree packages (identified elsewhere)
     # Retrieves package object based on string name
     nixpkgs.config.allowUnfreePredicate = pkg:
       builtins.elem (lib.getName pkg) config.unfreePackages;
-
-    # Pin a state version to prevent warnings
-    home-manager.users.${config.user} = {
-      # act
-      home.stateVersion = stateVersion;
-      programs.home-manager.enable = true;
-    };
-
-    home-manager.users.root.home.stateVersion = stateVersion;
   };
 }
 
--- modules/nixos/other/1password.nix
+++ modules/nixos/other/1password.nix
@@ -16,24 +16,23 @@ in
   
 
   config = mkIf cfg.enable {
-    # Enable the unfree 1Password packages
-    # nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    #   "1password-gui"
-    #   "1password"
-    # ];
-    # Alternatively, you could also just allow all unfree packages
-    nixpkgs.config.allowUnfree = true;
+    unfreePackages = [
+      "1password-gui"
+      "1password"
+    ];
 
-    programs._1password = {
-      enable = true;
-      package = pkgs._1password;
-    };
-    programs._1password-gui = {
-      enable = true;
-      # Certain features, including CLI integration and system authentication support,
-      # require enabling PolKit integration on some desktop environments (e.g. Plasma).
-      polkitPolicyOwners = [ config.user ];
-      package = pkgs._1password-gui;
+    programs = {
+      _1password = {
+        enable = true;
+        package = pkgs._1password;
+      };
+      _1password-gui = {
+        enable = true;
+        # Certain features, including CLI integration and system authentication support,
+        # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+        polkitPolicyOwners = [ config.user ];
+        package = pkgs._1password-gui;
+      };
     };
   };
 }
