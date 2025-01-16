12a725d4c5f485b74d70c4839512443b8d3bc917
Quade Mashaw <50602948+a-clean-kitchen@users.noreply.github.com>
2025-01-15 19:18:52 -0500

    Junkr prep (#5)
    Add things to necessary to deploy new laptop!

---
+++ hosts/Junkr/default.nix
@@ -0,0 +1,44 @@
+
+{ inputs, globals, overlays, ... }:
+
+inputs.nixpkgs.lib.nixosSystem {
+  system = "x86_64-linux";
+  modules = [
+    globals
+    inputs.disko.nixosModules.disko
+    inputs.home-manager.nixosModules.home-manager
+    ../../modules/common
+    ../../modules/nixos
+    ./hardware-configuration.nix
+    {
+      dotfiles.enable = true;
+      
+      laptop = true;
+      nixpkgs.overlays = overlays;      
+      publicKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMQcdy3fe9wP0zmx/TMPcZ3r4b38sitxg3ieTSkPbvju"
+      ];
+      services.openssh.enable = true;
+      networking.hostName = "junkr";
+     
+      # Boot from a usb
+      # Set password for root: sudo -s; passwd
+      # # Look in Makefile for an example init command once you have a root password and ip/hostname
+      disko = {
+        enableConfig = true;
+        devices = (import ../../disks/root.nix { disk = "/dev/sda"; });
+      };
+
+      # # I will never touch these
+      # boot.initrd.availableKernelModules = 
+      #   [ "ahci" "xhci_pci" "usb_storage" "sd_mod" ];
+      #
+      # boot.kernelModules = [ "kvm-intel" ];
+      # hardware.enableRedistributableFirmware = true;
+
+      time.timeZone = "America/New_York";
+      # A key of sorts
+      passwordHash = inputs.nixpkgs.lib.fileContents ../../misc/password.sha512;
+    }
+  ];
+}
