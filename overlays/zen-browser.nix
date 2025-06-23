2a87b5f3368d8077a426a6d297e1f11c90861760
a-clean-kitchen <mashaw.quade@gmail.com>
2025-06-23 16:40:39 -0400

    backup zen

--- overlays/zen-browser.nix
+++ overlays/zen-browser.nix
@@ -1,3 +1,10 @@
-inputs: _final: prev: {
+inputs: final: prev: {
   zen-browser = inputs.zen-browser.packages.${prev.system}.default;
+
+  # The Backup Route
+  # zen-browser-unwrapped = prev.callPackage ../pkgs/zen-browser.nix { };
+  # zen-browser = prev.wrapFirefox final.zen-browser-unwrapped {
+  #   pname = "zen-browser";
+  # };
 }
+
